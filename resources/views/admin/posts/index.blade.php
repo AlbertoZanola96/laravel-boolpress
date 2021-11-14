abf5ed07918890e5d11b1aee6af629f0db1cbf39
AlbertoZanola <albertozanola1996@gmail.com>
2021-11-14 15:03:39 +0100

    Conferma delete

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -36,7 +36,7 @@
                         class="btn btn-warning">
                         Modify
                     </a>
-                    <form class="delete-post-form" style="display: inline" method="post" action="{{ route('admin.posts.destroy', $post->id) }}">
+                    <form class="delete-post-form delete-post" style="display: inline" method="post" action="{{ route('admin.posts.destroy', $post->id) }}">
                         @csrf
                         @method('DELETE')
                         <button  type="submit" class="btn btn-danger ">Delete</button>
